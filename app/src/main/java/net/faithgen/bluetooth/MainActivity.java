91dc576165a157ee0c00d375ac7962f6df06a02d
innoflash <innocentmazando@gmail.com>
2019-11-10 21:52:30 +0200

    trying initializing bluetooth

--- app/src/main/java/net/faithgen/bluetooth/MainActivity.java
+++ app/src/main/java/net/faithgen/bluetooth/MainActivity.java
@@ -1,16 +1,18 @@
 package net.faithgen.bluetooth;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 
-import com.karumi.dexter.Dexter;
 import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
@@ -20,8 +22,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MultiplePermissionsListener {
 
+    private final static int REQUEST_ENABLE_BT = 1;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 2;
+    private static final long SCAN_PERIOD = 10000;
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothManager bluetoothManager;
+    private Intent enableBtIntent;
+    private Handler handler;
+    private BluetoothAdapter.LeScanCallback leScanCallback;
+    private boolean isScanning;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
@@ -29,17 +38,55 @@ public class MainActivity extends AppCompatActivity implements MultiplePermissio
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        leScanCallback = initScanCallback();
+
         bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else scanLeDevice(true);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private BluetoothAdapter.LeScanCallback initScanCallback() {
+        return (bluetoothDevice, i, bytes) -> {
+
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(() -> {
+                isScanning = false;
+                bluetoothAdapter.stopLeScan(leScanCallback);
+            }, SCAN_PERIOD);
+
+            isScanning = true;
+            bluetoothAdapter.startLeScan(leScanCallback);
+        } else {
+            isScanning = false;
+            bluetoothAdapter.stopLeScan(leScanCallback);
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Dexter.withActivity(this)
-                .withPermissions(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+/*        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
                 .withListener(this)
-                .check();
+                .check();*/
     }
 
     @Override
