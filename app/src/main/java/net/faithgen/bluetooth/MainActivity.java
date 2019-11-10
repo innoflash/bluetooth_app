fa60e11381801ffa5d8c493699a2ecce4dffc761
innoflash <innocentmazando@gmail.com>
2019-11-10 21:16:02 +0200

    added ios dialog and progress

--- app/src/main/java/net/faithgen/bluetooth/MainActivity.java
+++ app/src/main/java/net/faithgen/bluetooth/MainActivity.java
@@ -1,14 +1,54 @@
 package net.faithgen.bluetooth;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MultiplePermissionsListener {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothManager bluetoothManager;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(this)
+                .check();
+    }
+
+    @Override
+    public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+    }
+
+    @Override
+    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
     }
 }
